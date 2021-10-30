e0e1ad64603ab33e70d3df2b7329b453a9d69ce0
leevic31 <leevj.lee@mail.utoronto.ca>
2021-10-30 13:12:26 -0400

    changed date field

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get "/accounts"
+    assert_response :success
+  end
 end
